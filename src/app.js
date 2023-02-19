235a5bbb0a97648fc4effacaab7873ed6f343bee
RichardoTS <ri.tapiasaez@gmail.com>
2023-02-19 03:02:31 +0000

    Excuse Generator Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function getRandomItem(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    return item;
+  }
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let result =
+    getRandomItem(who) +
+    " " +
+    getRandomItem(action) +
+    " " +
+    getRandomItem(what) +
+    " " +
+    getRandomItem(when);
+  document.getElementById("excuse").innerHTML = result;
+
   console.log("Hello Rigo from the console!");
 };
